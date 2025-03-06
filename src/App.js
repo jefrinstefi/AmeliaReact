c27bd88562d9e0ec327dfc615f7f4d801d09a05f
Mahendravarma-S <mahe2kboy@gmail.com>
2025-03-06 14:43:17 +0530

    Created component  Folder and installed Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Upload from "./components/Upload/Upload";
+import Dashboard from "./components/Dashboard/Dashboard";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> <h1>Aj</h1>
-
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        
+      </Routes>
+    </Router>
   );
 }
 
